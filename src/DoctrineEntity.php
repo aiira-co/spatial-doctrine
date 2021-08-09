bda35e1b1179c6f62618402cbf4e9cbc652afa76
Kofi Owusu-Afriyie <airdesign.code@gmail.com>
2021-08-09 09:12:40 +0000

    adding get & set DoctrineConfig method

--- src/DoctrineEntity.php
+++ src/DoctrineEntity.php
@@ -9,7 +9,10 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
 use Doctrine\ORM\ORMException;
+
+//use Doctrine\Common\Cache;
 
 class DoctrineEntity
 {
@@ -76,8 +79,10 @@ class DoctrineEntity
 
         // I might need to force value of driver for domain folder at constructor
         // Driver Implementation
-        $driverImpl = $config
-            ->newDefaultAnnotationDriver($domainRootPath . $domain);
+//        $driverImpl = $config
+//            ->newDefaultAnnotationDriver($domainRootPath . $domain);
+//        use attribute(meta) as default
+        $driverImpl = new AttributeDriver([$domainRootPath . $domain]);
         $config->setMetadataDriverImpl($driverImpl);
 
         // Cache
@@ -93,19 +98,6 @@ class DoctrineEntity
         // return $this;
     }
 
-    public function getDoctrineConfig(): Configuration
-    {
-        return $this->_config;
-    }
-
-    /**
-     * @param \Doctrine\ORM\Configuration $config
-     * @return $this
-     */
-    public function setDoctrineConfig(Configuration $config): self
-    {
-        $this->_config = $config;
-    }
 
     /**
      * Return Doctrine's
@@ -115,12 +107,9 @@ class DoctrineEntity
      * @return EntityManager
      * @throws ORMException
      */
-    public
-    function entityManager(
-        array|Connection $connection,
-        ?Configuration $config = null
-    ): EntityManager {
-        return EntityManager::create($connection, $config ?? $this->_config);
+    public function entityManager(array|Connection $connection): EntityManager
+    {
+        return EntityManager::create($connection, $this->_config);
     }
 
     /**
@@ -129,10 +118,8 @@ class DoctrineEntity
      * @param boolean $dev
      * @return self
      */
-    public
-    function isDev(
-        bool $dev = false
-    ): self {
+    public function isDev(bool $dev = false): self
+    {
         if ($dev) {
 //            $this->_cache = new ArrayCache;
             $this->_config->setAutoGenerateProxyClasses(true);
@@ -143,7 +130,7 @@ class DoctrineEntity
         return $this;
     }
 
-// Proxies Directory
+    // Proxies Directory
 
     /**
      * Configuration Options
@@ -153,10 +140,8 @@ class DoctrineEntity
      * @param string|null $dir
      * @return self
      */
-    public
-    function setProxyDir(
-        ?string $dir
-    ): self {
+    public function setProxyDir(?string $dir): self
+    {
         if ($dir === null) {
             $dir = $this->relativeDirPath . 'src' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'domain' . DIRECTORY_SEPARATOR . 'proxies';
         }
@@ -166,7 +151,7 @@ class DoctrineEntity
     }
 
 
-// Proxy Namespace
+    // Proxy Namespace
 
     /**
      * Sets the namespace to use for generated proxy classes.
@@ -174,10 +159,8 @@ class DoctrineEntity
      * @param string $namespace
      * @return self
      */
-    public
-    function setProxyNamespace(
-        string $namespace = 'Core\Domain'
-    ): self {
+    public function setProxyNamespace(string $namespace = 'Core\Domain'): self
+    {
         $this->_config->setProxyNamespace($namespace);
         return $this;
     }
@@ -191,10 +174,8 @@ class DoctrineEntity
      * @param [type] $connectionOptions
      * @return self
      */
-    public
-    function setMetadataDriverImpl(
-        $driver
-    ): self {
+    public function setMetadataDriverImpl($driver): self
+    {
         $this->_config->setMetadataDriverImpl($driver);
         return $this;
     }
@@ -203,8 +184,7 @@ class DoctrineEntity
      * Dbal Types
      * @throws \Doctrine\DBAL\Exception
      */
-    private
-    function dbalTypes(): void
+    private function dbalTypes(): void
     {
         $dbalTypes = DoctrineConfig['doctrine']['dbal']['types'];
         if ($dbalTypes !== null && count($dbalTypes) > 0) {
